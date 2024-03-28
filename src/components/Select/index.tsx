9fa919750e8bc182164660bc1207b747117f0d3b
rayaneacacio <rray74307@gmail.com>
2024-03-28 16:10:51 -0300

    update html e css do componente Select

--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -1,11 +1,31 @@
-import { ReactElement } from "react";
+import { ReactElement, useState } from "react";
 import { Container } from "./style";
+import { IoIosArrowDown } from "react-icons/io";
 
 export default function Select(props: { defaultValue?: string }): ReactElement {
+  const [ value, setValue ] = useState<string>(props.defaultValue ?? "USD");
+
+  function handleOpenSelect(container: HTMLDivElement): void {
+    const options = container.querySelector(".options")! as HTMLDivElement;
+
+    if(options.style.display == "none") {
+      options.style.display = "block";
+    } else {
+      options.style.display = "none";
+    }
+  }
+
   return (
-    <Container defaultValue={ props.defaultValue }>
-      <option value="USD">USD</option>
-      <option value="BRL">BRL</option>
+    <Container onClick={(event) => handleOpenSelect(event.currentTarget) }>
+      <div className="select">
+        <p>{ value }</p>
+        <IoIosArrowDown size={ 25 } />
+      </div>
+
+      <div className="options">
+        <button onClick={() => setValue("USD") }>USD</button>
+        <button onClick={() => setValue("BRL") }>BRL</button>
+      </div>
     </Container>
   )
 }
--- src/components/Select/style.ts
+++ src/components/Select/style.ts
@@ -1,10 +1,53 @@
 import styled from "styled-components";
 
-export const Container = styled.select`
-  color: ${({ theme }) => theme.COLORS.GRAY};
-  padding: 1rem 4rem 1rem 1rem;
-  border-radius: 1rem;
+export const Container = styled.div`
+  width: 10rem;
+  color: ${({ theme }) => theme.COLORS.GRAY_2};
   border: 1px solid ${({ theme }) => theme.COLORS.GRAY};
+  border-radius: 1rem;
+  position: relative;
   cursor: pointer;
-  outline: 0;
+  -webkit-tap-highlight-color: transparent;
+
+  .select {
+    padding: 5px 1.5rem;
+    display: flex;
+    align-items: center;
+  }
+
+  svg {
+    position: absolute;
+    right: 1rem;
+  }
+
+  .options {
+    background-color: ${({ theme }) => theme.COLORS.WHITE};
+    width: 10rem;
+    position: absolute;
+    z-index: 1;
+    display: none;
+    border: 1px solid ${({ theme }) => theme.COLORS.GRAY};
+    border-radius: 1rem;
+
+    > button {
+      color: ${({ theme }) => theme.COLORS.BLACK};
+      width: 100%;
+      padding: 1rem;
+      text-align: left;
+
+      &:hover {
+        background-color: ${({ theme }) => theme.COLORS.GRAY_2};
+        color: ${({ theme }) => theme.COLORS.WHITE};
+        filter: none;
+      }
+    }
+  }
+
+  @media(min-width: 1000px) {
+    width: 13.5rem;
+
+    .options {
+      width: 13.5rem;
+    }
+  }
 `;
